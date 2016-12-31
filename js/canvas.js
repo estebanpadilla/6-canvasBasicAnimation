9c1c7af0c8d740dd6cefb8606db1a14f01d4c6c4
Esteban Padilla <ep@estebanpadilla.com>
2016-12-31 17:29:00 -0600

    Save.

--- js/canvas.js
+++ js/canvas.js
@@ -1,15 +1,15 @@
 window.addEventListener('load', init, false);
 
 function init() {
+
     var canvas = undefined;
     var context = undefined;
     canvas = createCanvas();
     context = canvas.getContext('2d');
 
-    var ball = new Ball(0, 400, 5, '#ee3344', context);
+    var ball = new Ball(0, window.innerHeight / 2, 50, '#ee3344', context);
 
     function update() {
-
         context.clearRect(0, 0, canvas.width, canvas.height);
 
         ball.update();
@@ -35,11 +35,8 @@ function Ball(x, y, radius, color, context) {
     this.context = context;
     this.x = x;
     this.y = y;
-    this.originY = y;
     this.radius = radius;
     this.color = color;
-    this.angle = 1;
-    this.waveLength = 0.1;
 }
 
 Ball.prototype.render = function render() {
@@ -51,22 +48,11 @@ Ball.prototype.render = function render() {
 
 Ball.prototype.update = function update() {
 
-    //range of values from -5 to 5
-    this.movement = Math.sin(this.angle) * 5;
+    this.x += 1;
 
-    this.y += this.movement;
-    this.x += 2;
-    this.angle += this.waveLength;
-
-    if (this.x > window.innerWidth) {
+    if (this.x > window.width) {
         this.x = 0;
-        this.color = 'rgb(' + myramdon(100, 255) + ',' + myramdon(0, 100) + ',' + myramdon(0, 100) + ')';
-        this.waveLength = myramdon(1, 10) / 10;
     }
 
     this.render();
 }
-
-function myramdon(min, max) {
-    return (Math.random() * (max - min + 1) | 0) + min;
-}  
