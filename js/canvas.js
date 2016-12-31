6c942611ccd4df721b08568b344db5b924083e98
Esteban Padilla <ep@estebanpadilla.com>
2016-12-30 18:06:57 -0600

    Save changes.

--- js/canvas.js
+++ js/canvas.js
@@ -6,13 +6,16 @@ function init() {
     canvas = createCanvas();
     context = canvas.getContext('2d');
 
+    var ball = new Ball(0, 400, 5, '#ee3344', context);
+
     function update() {
 
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
+        ball.update();
 
-        //requestAnimationFrame(update);
+        requestAnimationFrame(update);
     }
-
     update();
 }
 
@@ -27,3 +30,43 @@ function createCanvas() {
     document.body.appendChild(canvas);
     return canvas;
 }
+
+function Ball(x, y, radius, color, context) {
+    this.context = context;
+    this.x = x;
+    this.y = y;
+    this.originY = y;
+    this.radius = radius;
+    this.color = color;
+    this.angle = 1;
+    this.waveLength = 0.1;
+}
+
+Ball.prototype.render = function render() {
+    this.context.beginPath();
+    this.context.fillStyle = this.color;
+    this.context.arc(this.x, this.y, this.radius, 0, (Math.PI * 2), false);
+    this.context.fill();
+}
+
+Ball.prototype.update = function update() {
+
+    //range of values from -5 to 5
+    this.movement = Math.sin(this.angle) * 1;
+
+    this.y += this.movement;
+    this.x += 2;
+    this.angle += this.waveLength;
+
+    if (this.x > window.innerWidth) {
+        this.x = 0;
+        this.color = 'rgb(' + myramdon(100, 255) + ',' + myramdon(0, 100) + ',' + myramdon(0, 100) + ')';
+        this.waveLength = myramdon(1, 10) / 10;
+    }
+
+    this.render();
+}
+
+function myramdon(min, max) {
+    return (Math.random() * (max - min + 1) | 0) + min;
+}  
